8a4d3f33a1edbd6ec18c5fa6ce46f2e69fb5f960
Isaac Ledesma <isaacledesma76@gmail.com>
2022-11-01 20:26:47 -0600

    modelo ticket

---
+++ api/models/Ticket.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'; 
+
+const ticketSchema =  new mongoose.Schema({
+
+    projection: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Projection',
+
+    } 
+    ],
+    user: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+    }
+    ],
+    number_of_tickets: Number,
+    subtotal: Number,
+    discount: Number,
+    total: Number,
+
+})
+
+export default mongoose.model('Ticket', ticketSchema);
