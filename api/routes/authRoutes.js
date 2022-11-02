0749de8cb1d8d1a3ae6ffe8ab0e040735c817734
Miguel Calixto <miguelcalixto@wolfsellers.com>
2022-11-02 13:14:35 -0600

    middleware

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import  express  from "express";
 import {verifyUser} from "../controllers/userController.js"
-
+import { authValidator } from "../middlewares/authValidator.js";
 const router = express.Router();
 
-router.route("/verify").put(verifyUser);
+router.route("/verify").put(authValidator,verifyUser);
 export default router;
