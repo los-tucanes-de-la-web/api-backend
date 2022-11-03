a2b958299c0a929de6c38a50aaa9cbd046583a98
angel <angel.36b@gmail.com>
2022-11-02 21:48:19 -0500

    conflicto arreglado #100

--- api/api.js
+++ api/api.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
-<<<<<<< HEAD
 import userRouter from './routes/userRouter.js'
-=======
 
 import authRoutes from './routes/authRoutes.js'
 
->>>>>>> 64580be44c05a8a33acb602dd52e259440516c7a
 import movieRoutes from './routes/movieRoutes.js';
 import projectionRoutes from './routes/projectionRoutes.js';
 
@@ -27,11 +24,8 @@ api.use("/verify",authRoutes)
 //TODO:
 
 api.use('/movies', movieRoutes);
-<<<<<<< HEAD
 api.use('/users', userRouter );
-=======
 api.use('/projections', projectionRoutes);
->>>>>>> 34823d205fafb14e32c7d7f56cc91a6c33082242
 
 
 export default api;
