ae80f9993e955b0c4bedbc7534804570070fd74f
Miguel Calixto <miguelcalixto@wolfsellers.com>
2022-11-02 19:22:11 -0600

    Remove import

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,6 +1,5 @@
 import  express  from "express";
 import {verifyUser} from "../controllers/userController.js"
-import { authValidator } from "../middlewares/authValidator.js";
 const router = express.Router();
 
 router.route("/verify/:token").put(verifyUser);
