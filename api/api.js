3ef4396beb993fad2f42d7c243c621c09f48359c
Jonathan Arenas <jonathanarenas318@gmail.com>
2022-11-02 19:54:54 -0600

    the tickets route was registered in api

--- api/api.js
+++ api/api.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import ticketRoutes from './routes/ticketRoutes.js';
 
 const api = express();
 
 api.use(morgan('combined'));
 
 api.use(express.json());
-
+api.use('/tickets', ticketRoutes)
 api.get('/status', (_, res) => {
   res.json({
     msg: 'API En linea funcionado',
