2fc06ca300512e8fbd8c2b4afb75eae0b743617a
RicardoSanchez <taciturnosotros@gmail.com>
2022-11-03 21:20:57 -0600

    Se hizo el update de Cinema :)

--- api/routes/cinemaRoutes.js
+++ api/routes/cinemaRoutes.js
@@ -4,8 +4,12 @@ import * as cinemaController from '../controllers/cinemaController.js'
 const router = express.Router();
 
 router
-  .route('/:id').delete(cinemaController.remove)
+.route('/').post(cinemaController.create);
 
-  .route('/').post(cinemaController.create);
+router
+  .route('/:id')
+  .delete(cinemaController.remove)
+  .put(cinemaController.update)
+  
 
 export default router;
