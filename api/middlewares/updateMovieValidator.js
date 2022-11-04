6a7c6ad4b530472d64174106707a2c189362966b
Miguel Calixto <miguelcalixto@wolfsellers.com>
2022-11-03 20:42:00 -0600

    conflict

--- api/middlewares/updateMovieValidator.js
+++ api/middlewares/updateMovieValidator.js
@@ -2,7 +2,6 @@ import joi from 'joi'
 
 const updateMovieValidator = async (req, res, next) => {
     const movieSchema = joi.object({
-<<<<<<< HEAD
         title: joi.string().required(),
         director: joi.array().items(joi.string()).min(1).required(),
         producer: joi.array().items(joi.string()).min(1).required(),
@@ -13,18 +12,6 @@ const updateMovieValidator = async (req, res, next) => {
         synopsis: joi.string().required(),
         score: joi.number().required(),
         poster: joi.string().required()
-=======
-        title: joi.string(),
-        director: joi.array().min(1),
-        producer: joi.array().min(1),
-        genre: joi.array().min(1),
-        cast: joi.array().min(1),
-        releaseDate: joi.date(),
-        duration: joi.number().greater(60),
-        synopsis: joi.string(),
-        score: joi.number(),
-        poster: joi.string()
->>>>>>> fc8cef0 (Movie update validator)
     })
 
     try {
